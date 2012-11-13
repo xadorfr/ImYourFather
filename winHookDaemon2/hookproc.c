c6d9071f273468375006662ad7918b16b2fddd7b
xderoche <xderoche@laptop-147-210-128-133.labri.fr>
2012-11-13 09:22:21 +0100

    winHookDaemon - adaptation pour compilation GNU - TODO : serveur select

---
+++ winHookDaemon2/hookproc.c
@@ -0,0 +1,113 @@
+/**
+ * Important : HookProc() is a Hook fonction, i.e. executed by a thread of the application which is creating, activating and destroying a window
+ */
+#include <stdio.h>
+#include "hookproc.h"
+
+/* TODO (todo tag - code search : #MultiClient)
+ * atm the DLL is not prepared to be used by several client programs at the same time
+ * a solution would be a collection of thread id : thread id of client programs which await for window's handles
+ * In between time, the boolean "loaded" avoid multi programs attachment 
+ */
+#pragma data_seg(".shared")
+BOOL loaded = FALSE;
+DWORD threadId = 0;
+#pragma data_seg()
+#pragma comment(linker, "/SECTION:.shared,RWS") // /!\ very important to make shared variable available for all DLL instances
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH: {
+		/* TODO#MultiClient */
+		if(loaded == TRUE){
+			return FALSE;
+		}
+		loaded = TRUE;
+		break;
+							 }
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH: {
+		if(loaded) {
+			loaded = FALSE;
+		}
+		break;
+							 }
+	}
+	return TRUE;
+}
+
+/*
+ * wParam : not used
+ * lParam : A pointer to a CWPSTRUCT structure that contains details about the message. 
+ * CWPSTRUCT description : http://msdn.microsoft.com/en-us/library/windows/desktop/ms644964%28v=vs.85%29.aspx
+ */
+LRESULT CALLBACK HookProc (int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode < 0) {
+		return CallNextHookEx(NULL, nCode, wParam, lParam);
+	}
+
+	switch(nCode) {
+
+	case HCBT_CREATEWND: {
+		// tag the created window
+		HWND win = (HWND) wParam;
+		SetProp(win, WIN_TAG_CREATED, win); 
+		break;
+						 }
+	case HCBT_ACTIVATE: {
+		/* WPARAM -> handle to the new window
+		 LPARAM -> long pointer to a CBT_CREATEWND structure */
+
+		/* check tag */
+		HWND win = (HWND) wParam;
+		if (GetProp(win, WIN_TAG_CREATED) == NULL) {
+			// if the tag tagCreated isn't detected, it means that the activated window has not been freshly created
+			break;
+		}
+		RemoveProp(win, WIN_TAG_CREATED);
+
+		CBTACTIVATESTRUCT* actStruct = (CBTACTIVATESTRUCT*) lParam;
+		if(! actStruct->fMouse) { // not activate by a mouse action
+			/* TODO#MultiClient */
+			if(threadId != 0) {
+				PostThreadMessage(threadId, CUSTOM_MSG_CREATION, (WPARAM) win, (LPARAM) NULL);
+				// TODO : find out how to tag it from java (in order to tag reparented windows only)
+				SetProp(win, WIN_TAG_REPARENTED, win);
+			}
+		}
+		break;
+						}
+
+	case HCBT_DESTROYWND: {
+		HWND win = (HWND) wParam;
+		/* TODO#MultiClient */
+		if (GetProp(win, WIN_TAG_REPARENTED) == NULL || threadId == 0) {
+			// the only windows we are supposed to be interrested in are the one which are reparented
+			break;
+		}
+		PostThreadMessage(threadId, CUSTOM_MSG_DESTRUCTION, (WPARAM) win, (LPARAM) NULL);
+						  }
+	}
+
+	// msdn : "If the hook procedure returns a nonzero value, the system destroys the window"
+	return CallNextHookEx(NULL, nCode, wParam, lParam);	
+}
+
+/*
+ * This setter is used in order to suscribe to notifications : 
+ * any PostThreadMessage() in HookProc()
+ * 
+ * TODO#MultiClient : change this setter by a "addClientThreadId" function 
+ * in order to make this DLL useable by several programs
+ */
+void HookProcSetTid(DWORD tid) {
+	threadId = tid;
+}
---
+++ winHookDaemon2/hookproc.h
@@ -0,0 +1,26 @@
+#ifndef HOOKPROC_H
+#define HOOKPROC_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <windows.h>
+
+LRESULT CALLBACK HookProc (int nCode, WPARAM wParam, LPARAM lParam);
+void HookProcSetTid(DWORD tid);
+
+/** constant */
+
+const UINT CUSTOM_MSG_CREATION = WM_APP + 1;
+const UINT CUSTOM_MSG_DESTRUCTION = WM_APP + 2;
+
+const LPCSTR WIN_TAG_CREATED = "JUSTCREATED";
+const LPCSTR WIN_TAG_REPARENTED = "REPARENTED";
+
+#ifdef __cplusplus
+} // __cplusplus defined.
+#endif
+
+#endif
